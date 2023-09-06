a679294f1db41135cdbff99823e35df6971ec995
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2023-09-06 14:32:25 +0300

    Update FirstCode.cpp

--- FirstCode.cpp
+++ FirstCode.cpp
@@ -7,6 +7,7 @@ int main()
 {
  int a = 3;
  double b = 0.501;
+ double result = 0;
  
  double first = pow(pow((a-b), 2) * pow(sin (1 - a/b), 2) * pow(M_PI / 6, 2) * pow(cos (1 - b/a), 2), 1./3);
  double second = 0.701 * pow(log(a - b), 2);
