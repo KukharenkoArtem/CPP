8484b7f3ed5355652249655878d8be6df7c6b331
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2023-09-13 08:53:06 +0300

    Create Task1.cpp

---
+++ 06.09/Task1.cpp
@@ -0,0 +1,18 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <math.h>
+
+int main()
+{
+ int a = 3;
+ double b = 0.501;
+ double result = 0;
+ 
+ double first = pow(pow((a-b), 2) * pow(sin (1 - a/b), 2) * pow(M_PI / 6, 2) * pow(cos (1 - b/a), 2), 1./3);
+ double second = 0.701 * pow(log(a - b), 2);
+ 
+ result = first/second;
+ 
+ std::cout << result;
+}
