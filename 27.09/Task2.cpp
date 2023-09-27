c785e317096d15c9a69a2fab74d9e5941188ec8b
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2023-09-27 15:49:44 +0300

    Update Task2.cpp

--- 27.09/Task2.cpp
+++ 27.09/Task2.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+float a1 = 2.3, a2 = 2.3, a3 = 2.123456, a4 = 2.123456, a5 = 2.123456;
+cout << setprecision (2) << a1 << '\n';
+cout << setprecision (3) << a2 << '\n';
+cout << setprecision (7) << a3 << '\n';
+cout << setprecision (3) << a4 << '\n';
+cout << setprecision (1) << a5 << '\n';
+}
