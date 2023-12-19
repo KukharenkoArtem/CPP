3158be03f20a1d66a1f4e060d243b0057f5c01f2
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2023-12-19 21:27:02 +0200

    Create Task5.cpp

---
+++ 11.08/Task5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    const int m = 4;
+    const int n = 7;
+
+    int mas[m][n] = {{28, 11, 88, 34}, {4, 50, 17, 9}, {3, 128, 19, 72}};
+
+    int maxElement = mas[0][0];
+    int maxRow = 1, maxColumn = 1;
+
+    for (int row = 0; row < m; ++row) {
+        for (int column = 0; column < n; ++column) {
+            if (mas[row][column] > maxElement) {
+                maxElement = mas[row][column];
+                maxRow = row + 1;
+                maxColumn = column + 1;
+            }
+        }
+    }
+
+    cout << "Maximum element: " << maxElement << endl;
+    cout << "Row number: " << maxRow << ", Column number: " << maxColumn << endl;
+}
