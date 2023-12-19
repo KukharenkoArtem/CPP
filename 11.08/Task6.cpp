707886654adc95376f79f0760e23237d56e0c8d9
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2023-12-19 21:28:00 +0200

    Create Task6.cpp

---
+++ 11.08/Task6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    const int m = 3;
+    const int n = 4;
+
+    int mas[m][n] = {{28, 11, 88, 34}, {4, 50, 17, 9}, {3, 128, 19, 72}};
+
+    int minElement = mas[0][0];
+    int minRow = 1, minColumn = 1;
+
+    for (int row = 0; row < m; ++row) {
+        for (int column = 0; column < n; ++column) {
+            if (mas[row][column] < minElement) {
+                minElement = mas[row][column];
+                minRow = row + 1;
+                minColumn = column + 1;
+            }
+        }
+    }
+
+    cout << "Minimum element: " << minElement << endl;
+    cout << "Row number: " << minRow << ", Column number: " << minColumn << endl;
+}
