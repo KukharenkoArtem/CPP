a3552acb8d12b70bc1c383735d78f6011b20bd0e
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2023-12-19 21:29:39 +0200

    Create Task15.cpp

---
+++ 11.08/Task15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int m = 3;
+    int matrix[m][m] = {
+        {2, 5, 8},
+        {4, 7, 1},
+        {6, 3, 9}
+    };
+
+    int sumMainDiagonal = 0;
+    int sumSecondaryDiagonal = 0;
+
+    for (int i = 0; i < m; ++i) {
+        sumMainDiagonal += matrix[i][i];
+        sumSecondaryDiagonal += matrix[i][m - i - 1];
+    }
+
+    cout << "Сума по головній діагоналі: " << sumMainDiagonal << endl;
+    cout << "Сума по побічній діагоналі: " << sumSecondaryDiagonal << endl;
+}
