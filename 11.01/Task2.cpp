be12fb76eab90f31de2abc441b936b51d4d4a99c
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2023-12-20 08:54:13 +0200

    Create Task2.cpp

---
+++ 11.01/Task2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int size = 10;
+    int arr[size];
+
+    cout << "Enter " << size << " integers:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    int maxElement = arr[0];
+    int maxIndex = 0;
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+            maxIndex = i;
+        }
+    }
+
+
+    for (int i = maxIndex + 1; i < size; i++) {
+        if (arr[i] < 0) {
+        arr[i] = 0;
+    } else {
+        break;
+    }
+}
+
+    cout << "Updated array:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
