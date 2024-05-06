a6c3ca72bec20839392869e08f3626cf471de4f9
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2024-05-06 17:18:42 +0300

    Update Task2.cpp

--- Semester2/03.02/Task2.cpp
+++ Semester2/03.02/Task2.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int main() {
+  // Розмір масиву
+  const int rows = 3;
+  const int cols = 3;
+
+  // Динамічне виділення пам'яті для масиву
+  int** matrix = new int*[rows];
+  for (int i = 0; i < rows; i++) {
+    matrix[i] = new int[cols];
+  }
+
+  srand(time(NULL));
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      matrix[i][j] = rand() % 100;
+    }
+  }
+
+  cout << "Масив:\n";
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  cout << "\nЕлементи на головній діагоналі:\n";
+  for (int i = 0; i < rows; i++) {
+    cout << matrix[i][i] << " ";
+  }
+
+  cout << "\nЕлементи на побічній діагоналі:\n";
+  for (int i = 0; i < rows; i++) {
+    cout << matrix[i][rows - i - 1] << " ";
+  }
+
+  int evenCount = 0, oddCount = 0;
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      if (matrix[i][j] % 2 == 0) {
+        evenCount++;
+      } else {
+        oddCount++;
+      }
+    }
+  }
+
+  cout << "\nКількість парних елементів: " << evenCount << endl;
+  cout << "Кількість непарних елементів: " << oddCount << endl;
+
+  for (int i = 0; i < rows; ++i) {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
+
+  return 0;
+}
