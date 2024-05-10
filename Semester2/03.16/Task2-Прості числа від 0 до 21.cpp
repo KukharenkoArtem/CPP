34a1c81d22ebc0adc3acb89e660645bd16218232
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2024-05-10 09:05:51 +0300

    Update Task2-Прості числа від 0 до 21.cpp

--- Semester2/03.16/Task2-Прості числа від 0 до 21.cpp
+++ Semester2/03.16/Task2-Прості числа від 0 до 21.cpp
@@ -8,7 +8,7 @@ bool isPrime(int num) {
     return false;
   }
   
-  for (int i = 2; i <= sqrt(num); ++i) {
+  for (int i = 2; i <= sqrt(num); i++) {
     if (num % i == 0) {
       return false;
     }
