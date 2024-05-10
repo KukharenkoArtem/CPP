eebec9ff3c3ce0b8fb3703ff982ab86eddd16bc2
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2024-05-10 09:43:44 +0300

    Update and rename Task1.cpp to Task1-Валідатор пароля.cpp

---
+++ Semester2/03.23/Task1-Валідатор пароля.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string password;
+    getline(cin, password);
+
+    bool hasLength = password.length() >= 8;
+    bool hasUpperCase = false;
+    bool hasLowerCase = false;
+    bool hasDigit = false;
+    bool hasSpecialChar = false;
+
+    for (char ch : password) {
+        if (isupper(ch)) {
+            hasUpperCase = true;
+        } else if (islower(ch)) {
+            hasLowerCase = true;
+        } else if (isdigit(ch)) {
+            hasDigit = true;
+        } else if (!isalnum(ch)) {
+            hasSpecialChar = true;
+        }
+    }
+  
+    if (hasLength && hasUpperCase && hasLowerCase && hasDigit && hasSpecialChar) {
+        cout << "Password is correct." << endl;
+    } else {
+        cout << "Пароль is incorrect:" << endl;
+        if (!hasLength) {
+            cout << " - Password must contain at least 8 symbols." << endl;
+        }
+        if (!hasUpperCase) {
+            cout << " - Password must contain at least 1 uppercase letter." << endl;
+        }
+        if (!hasLowerCase) {
+            cout << " - Password must contain at least 1 lowercase letter." << endl;
+        }
+        if (!hasDigit) {
+            cout << " - Password must contain at least 1 number." << endl;
+        }
+        if (!hasSpecialChar) {
+            cout << " - Password must contain at least 1 speciak symbol." << endl;
+        }
+    }
+
+    return 0;
+}
