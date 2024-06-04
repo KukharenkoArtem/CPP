5bb29a2820e03b5db828d09c20599b008e197c47
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2024-06-04 23:48:56 +0300

    Create Task2.cpp

---
+++ Semester2/05.25/Task2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int main(void) {
+    int a, b, c;
+
+    cout << "Enter the first number: ";
+    cin >> a;
+
+    cout << "Enter the second number (except 0): ";
+    cin >> b;
+
+    try {
+        c = a / b;
+        cout << "Result: " << c << endl;
+    } catch (const runtime_error& ex) {
+        cerr << "Error: " << ex.what() << endl;
+        cerr << "Reason: Division by zero is not allowed." << endl;
+    }
+
+    return 0;
+}
