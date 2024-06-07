3727637c92c315f864253d2585788c355dc8bc03
KukharenkoArtem <88966152+KukharenkoArtem@users.noreply.github.com>
2024-06-07 15:14:38 +0300

    Update IP.cpp

--- Semester2/LastProjrects/IP.cpp
+++ Semester2/LastProjrects/IP.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class IPAddress {
+protected:
+    string ip;
+
+public:
+    IPAddress(const string& address) : ip(address) {}
+    IPAddress(const IPAddress& other) : ip(other.ip) {}
+
+    void print() const {
+        cout << ip << endl;
+    }
+};
+
+class IPAddressChecked : public IPAddress {
+private:
+    bool correct;
+
+public:
+    IPAddressChecked(const string& address, bool isCorrect) : IPAddress(address), correct(isCorrect) {}
+
+    IPAddressChecked(const IPAddressChecked& other) : IPAddress(other), correct(other.correct) {}
+
+    void print() const {
+        IPAddress::print();
+        cout << (correct ? " - Correct" : " - Not Correct") << endl;
+    }
+};
+
+int main() {
+    IPAddress ip1("1.2.3.4");
+    IPAddressChecked ip2("999.29.29.29", false);
+    IPAddressChecked ip3("199.29.29.29", true);
+
+    ip1.print();
+    ip2.print();
+    ip3.print();
+
+    return 0;
+}
